f1905671e2511b05788fceec07bc96705fd048d4
FENYUN323 <52124741+FENYUN323@users.noreply.github.com>
2020-12-30 20:29:44 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'IR_Test_2' 
+ * Target:  'IR_Test_2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
